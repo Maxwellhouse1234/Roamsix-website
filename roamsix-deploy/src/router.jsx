74997648b02233271daf262cd6723b5b9efef619
Maxwellhouse1234 <Max@reciprofy.com>
2026-04-08 18:53:17 -0700

    Refactor routing to use createBrowserRouter

--- roamsix-deploy/src/router.jsx
+++ roamsix-deploy/src/router.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProvingGroundsPage from './pages/ProvingGroundsPage';
 import ProvingGroundsRegistration from './pages/ProvingGroundsRegistration';
 import RetreatApplication from './pages/RetreatApplication';
 
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/proving-grounds" element={<ProvingGroundsPage />} />
-        <Route path="/proving-grounds/register" element={<ProvingGroundsRegistration />} />
-        <Route path="/retreat" element={<RetreatApplication />} />
-      </Routes>
-    </Router>
-  );
-}
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+  {
+    path: '/proving-grounds',
+    element: <ProvingGroundsPage />
+  },
+  {
+    path: '/proving-grounds/register',
+    element: <ProvingGroundsRegistration />
+  },
+  {
+    path: '/retreat',
+    element: <RetreatApplication />
+  }
+]);
